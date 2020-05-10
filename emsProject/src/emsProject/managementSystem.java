1bb44f3cc11c912a37e75b1cf5c385ffa98c7fc9
EngimaticGiant <tidus.tavis@gmail.com>
2020-05-10 15:00:37 -0500

    Start of project

---
+++ emsProject/src/emsProject/managementSystem.java
@@ -0,0 +1,10 @@
+package emsProject;
+
+public class managementSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
