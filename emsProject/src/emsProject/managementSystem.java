ba0b519fac11c47851003428ef9d4a1994c82d32
EngimaticGiant <tidus.tavis@gmail.com>
2020-05-16 01:31:48 -0500

    Connected to mySQL database. Small steps...

--- emsProject/src/emsProject/managementSystem.java
+++ emsProject/src/emsProject/managementSystem.java
@@ -1,10 +1,25 @@
 package emsProject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class managementSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		databaseConnector base = new databaseConnector();
+		base.connection = connect(base);
 		System.out.println("Hello world part two");
+	}
+	
+	private static Connection connect(databaseConnector d){
+		try {
+			d.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ems", "root", "root");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return d.connection;
 	}
 
 }
